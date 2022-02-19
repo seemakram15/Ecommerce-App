883648611695933ff6a9cc4ee0fbbad84cfc0456
Waseem Akram <seemakram15@gmail.com>
2022-02-19 22:29:08 +0500

    Precompile assets

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,5 @@ module EcommerceClone
     # Do not swallow errors in after_commit/after_rollback callbacks.
     # config.active_record.raise_in_transactional_callbacks = true
     config.load_defaults 5.2
-
   end
 end
