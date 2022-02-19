9e19c1ab96e38e776e0df5960a43b94a09c5d7f4
Waseem Akram <seemakram15@gmail.com>
2022-02-20 02:33:15 +0500

    CRUD operations are performed on Product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  # before_action :set_user_and_produt, only: %i[new create edit update delete_product]
 
   def new
     @user = User.find(params[:user_id])
@@ -15,6 +16,23 @@ class ProductsController < ApplicationController
     @user = current_user
     @products = @user.products
   end
+
+  def edit
+    @user = User.find(params[:user_id])
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @user = User.find(params[:user_id])
+    redirect_to user_product_path(user_id: @user.id, id: @product.id), notice: 'Product was successfully updated.' if @product.update(product_params)
+  end
+
+  def delete_product
+    @user = User.find(params[:user_id])
+    @product = Product.find(params[:id])
+    redirect_to user_products_path(user_id: @user.id), notice: 'Product was successfully deleted.' if @product.destroy
+   end
 
   def show
     @user = User.find(params[:user_id])
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   root 'wellcome#index', as: :authenticated_root
   devise_for :users
   resources :users do
-    resources :products
+    resources :products do
+      member do
+      delete :delete_product
+      end
+    end
   end
   scope :product do
     resources :categories, only: [:show, :create]
